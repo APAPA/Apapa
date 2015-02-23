f8a9688f4a6a85993e4c802cfa252f9f91333e89
RyanCccc <chenrd769@gmail.com>
2015-02-22 21:46:47 -0800

    Created new empty project

---
+++ GoEat/GoEat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GoEat
+//
+//  Created by Rendong Chen on 15/2/22.
+//  Copyright (c) 2015年 Apapa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
